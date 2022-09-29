ad752711ff44651cf5f6a9eb2e574ffd288dd8f5
PygmalionFR <ludovic.streetz@outlook.fr>
2022-09-29 18:13:32 +0200

    ajouts de la commande btc

---
+++ apikey.py
@@ -0,0 +1,1 @@
+key = "b653ddbc-30bc-4cc9-8f2a-775598c3a0fe"
--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-import os
 import disnake
-from dotenv import load_dotenv
+import apikey
+import requests
 from disnake.ext import commands
 
-load_dotenv(dotenv_path=".env/config")
 intents = disnake.Intents.all()
 
 bot = commands.Bot(
     command_prefix="!",
     intents=disnake.Intents.all()
 )
+
+headers = {
+    'X-CMC_PRO_API_KEY': apikey.key,
+    'accepts': 'application/json'
+}
+
+btc = {
+    'start': '1',
+    'limit': '1',
+    'convert': 'EUR'
+}
+
+url = ' https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+json = requests.get(url, params=btc, headers=headers).json()
+
+
+coins = json['data']
+
+coins[0]
+
+for x in coins:
+    if x['symbol'] == 'BTC':
+        token = x['quote']['EUR']['price']
+    print(x["symbol"], x['quote']['EUR']['price'])
 
 
 @bot.event
@@ -57,7 +81,8 @@ async def ban(ctx, user: disnake.User, *reason):
     reason = " ".join(reason)
     embed = disnake.Embed(title="**Banissement**", description="Un modérateur a frappe !")
     embed.set_author(name=ctx.author.name)
-    embed.set_thumbnail(url="https://discords.com/_next/image?url=https%3A%2F%2Fcdn.discordapp.com%2Femojis%2F849656179863977984.gif%3Fv%3D1&w=64&q=75")
+    embed.set_thumbnail(
+        url="https://discords.com/_next/image?url=https%3A%2F%2Fcdn.discordapp.com%2Femojis%2F849656179863977984.gif%3Fv%3D1&w=64&q=75")
     embed.add_field(name="Modérateur", value=ctx.author.name, inline=False)
     embed.add_field(name="Membre banni", value=user.name, inline=False)
     embed.add_field(name="Raison du ban", value=reason, inline=False)
@@ -70,7 +95,8 @@ async def kick(ctx, user: disnake.User, *reason):
     reason = " ".join(reason)
     embed = disnake.Embed(title="**Kick**", description="Un modérateur à kick!")
     embed.set_author(name=ctx.author.name)
-    embed.set_thumbnail(url="https://discords.com/_next/image?url=https%3A%2F%2Fcdn.discordapp.com%2Femojis%2F723096791230316544.png%3Fv%3D1&w=64&q=75")
+    embed.set_thumbnail(
+        url="https://discords.com/_next/image?url=https%3A%2F%2Fcdn.discordapp.com%2Femojis%2F723096791230316544.png%3Fv%3D1&w=64&q=75")
     embed.add_field(name="Modérateur", value=ctx.author.name, inline=False)
     embed.add_field(name="Membre kick", value=user.name, inline=False)
     embed.add_field(name="Raison du ban", value=reason, inline=False)
@@ -78,4 +104,8 @@ async def kick(ctx, user: disnake.User, *reason):
     await ctx.guild.kick(user, reason=reason)
 
 
-bot.run(os.getenv("TOKEN"))
+@bot.command()
+async def btc(ctx):
+    await ctx.send(f'Le Bitcoin est à : ||{token}|| €')
+
+bot.run("MTAyMTgxMDE2ODMxODY1NjUzNA.Gusfuy.edFOK0xhSRs0l_ymbatwk075qKwqYTqM1sbFBY")
